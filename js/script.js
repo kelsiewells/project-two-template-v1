dfa119a3f5c5a4e2354de2bb0887f9e2a1cf17fc
kelsiewells <59620231+kelsiewells@users.noreply.github.com>
2020-03-05 11:37:54 -0800

    tryna scroll here

--- js/script.js
+++ js/script.js
@@ -6,7 +6,53 @@ $(function() {
 
 
 
+var scale = Math.min( 
+  availableWidth / contentWidth, 
+  availableHeight / contentHeight 
+);
 
+var pageWidth, pageHeight;
+
+var basePage = {
+  width: 800,
+  height: 600,
+  scale: 1,
+  scaleX: 1,
+  scaleY: 1
+};
+
+$(function(){
+  var $page = $('.grid-item');
+  
+  getPageSize();
+  scalePages($page, pageWidth, pageHeight);
+  
+  //using underscore to delay resize method till finished resizing window
+  $(window).resize(_.debounce(function () {
+    getPageSize();            
+    scalePages($page, pageWidth, pageHeight);
+  }, 150));
+  
+
+function getPageSize() {
+  pageHeight = $('#grid-container').height();
+  pageWidth = $('#grid-container').width();
+}
+
+function scalePages(page, maxWidth, maxHeight) {            
+  var scaleX = 1, scaleY = 1;                      
+  scaleX = maxWidth / basePage.width;
+  scaleY = maxHeight / basePage.height;
+  basePage.scaleX = scaleX;
+  basePage.scaleY = scaleY;
+  basePage.scale = (scaleX > scaleY) ? scaleY : scaleX;
+
+  var newLeftPos = Math.abs(Math.floor(((basePage.width * basePage.scale) - maxWidth)/2));
+  var newTopPos = Math.abs(Math.floor(((basePage.height * basePage.scale) - maxHeight)/2));
+
+  page.attr('style', '-webkit-transform:scale(' + basePage.scale + ');left:' + newLeftPos + 'px;top:' + newTopPos + 'px;');
+}
+});
 
 
 
